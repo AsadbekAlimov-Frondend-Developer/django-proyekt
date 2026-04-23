65731a3cfa809870ff1780e937fe040ce4f20662
Asadbek <a37335877@gmail.com>
2026-04-23 05:20:21 +0500

    Update admin_setup.py

--- test/admin_setup.py
+++ test/admin_setup.py
@@ -1,11 +1,11 @@
 from flask import request, Response, current_app
-from flask_admin import Admin
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from models import Category, Product
 from werkzeug.exceptions import HTTPException
 
 # Класс для защиты админки паролем (Basic Auth)
-class SecureModelView(ModelView):
+class SecureViewMixin:
     def is_accessible(self):
         auth = request.authorization
         if auth and auth.username == current_app.config['ADMIN_USERNAME'] and \
@@ -20,6 +20,12 @@ class SecureModelView(ModelView):
                 {'WWW-Authenticate': 'Basic realm="Login Required"'}
             )
 
+class SecureModelView(SecureViewMixin, ModelView):
+    pass
+
+class SecureAdminIndexView(SecureViewMixin, AdminIndexView):
+    pass
+
 class ProductAdmin(SecureModelView):
     # Явно указываем, какие поля показывать при создании/редактировании товара
     form_columns = ['name', 'category', 'description', 'price', 'image_url']
@@ -29,7 +35,12 @@ class CategoryAdmin(SecureModelView):
 
 def init_admin(app, db):
     # Создаем админ-панель. Доступ по ссылке: http://127.0.0.1:5000/admin
-    admin = Admin(app, name='Управление магазином', template_mode='bootstrap4')
+    admin = Admin(
+        app, 
+        name='Управление магазином', 
+        template_mode='bootstrap4',
+        index_view=SecureAdminIndexView(name='Главная', url='/admin')
+    )
     
     # Добавляем разделы в админ-панель:
     admin.add_view(CategoryAdmin(Category, db.session, name="Категории"))
